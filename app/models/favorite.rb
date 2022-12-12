533417e9145cd5ccf81de22e5001a1995913a27b
mbraganza1 <mbraganza@uchicago.edu>
2022-12-12 15:54:24 +0000

    Okay so now the favorites work it actually works

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -9,6 +9,8 @@
 #  user_id    :integer
 #
 class Favorite < ApplicationRecord
-  belongs_to :user
-  belongs_to :moment
+  belongs_to :user, :required => true
+  belongs_to :moment, :required => true
+
+  validates :moment_id, :presence => true
 end
